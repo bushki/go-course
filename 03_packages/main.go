2728459f5c21ffed921dc3ced4c31d5c2360934a
Luis M <melgar.luis@gmail.com>
2019-03-03 14:37:20 -0800

    Importing custom packages

--- 03_packages/main.go
+++ 03_packages/main.go
@@ -4,10 +4,13 @@ package main
 import (
 	"fmt"
 	"math"
+
+	"github.com/bushki/go_course/03_packages/strutil"
 )
 
 func main() {
 	fmt.Println(math.Floor(2.71))
 	fmt.Println(math.Ceil(2.71))
 	fmt.Println(math.Sqrt(16))
+	fmt.Println(strutil.Reverse("hello"))
 }
