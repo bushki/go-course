42031abb77583e5c13f31bfb1be324ca0d11a808
Luis M <melgar.luis@gmail.com>
2019-03-03 15:02:47 -0800

    Arrays

---
+++ 05_arrays_slices/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Helo World")
+	//Arrays (fixed)
+	var fruitArr [2]string
+
+	//Assig values
+	fruitArr[0] = "apple"
+	fruitArr[1] = "orange"
+
+	fmt.Println(fruitArr)
+	fmt.Println(fruitArr[1])
+
+	//Declare and assign
+	myArr := [2]string{"banana", "grape"}
+	fmt.Println(myArr)
+
+	//Array Slice (size not fixed)
+	arrSlice := []string{"Sliced banana", "grape", "apple", "orange"}
+	fmt.Println(arrSlice)
+
+	//len
+	fmt.Println(len(arrSlice))
+
+	//range
+	fmt.Println(arrSlice[1:3]) //start at 1, do not include 3
+
+}
