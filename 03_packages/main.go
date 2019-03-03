cd895d501dca33622b1f1d3f428c0cb68bf74f00
Luis M <melgar.luis@gmail.com>
2019-03-03 14:31:57 -0800

    Importing multiple packages

---
+++ 03_packages/main.go
@@ -0,0 +1,13 @@
+package main
+
+// use parentheses to import multiple
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Floor(2.71))
+	fmt.Println(math.Ceil(2.71))
+	fmt.Println(math.Sqrt(16))
+}
